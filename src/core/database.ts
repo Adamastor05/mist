14551acb975debae668ae25da97571f55085ea05
Miguel <adamastorpequenortx@gmail.com>
2026-05-11 05:42:18 -0300

    implementa a criação do banco de dados

--- src/core/database.ts
+++ src/core/database.ts
@@ -1,1 +1,16 @@
+import { Database as IDatabase, SchemaTable } from "../types"
 
+export function createDatabase(schemasTables: { [key: string]: SchemaTable }): IDatabase {
+  const database: IDatabase = {
+    tables: {}
+  }
+
+  for (let schemaTable of Object.values(schemasTables)) {
+    database.tables[schemaTable.__nameTable] = {
+      config: schemaTable,
+      data: []
+    }
+  }
+
+  return database
+}
