7c6ffd5c33b660b667045e3de2092b6a23b334b5
Miguel <adamastorpequenortx@gmail.com>
2026-05-11 04:46:11 -0300

    feat: implementa a criação de colunas

---
+++ src/core/column.ts
@@ -0,0 +1,34 @@
+import { ConfigColumn, DataType } from "../types";
+
+class Column {
+  config: ConfigColumn;
+
+  constructor (name: string, dataType: DataType) {
+    this.config = {
+      name: name,
+      dataType: dataType,
+      notNull: false,
+      unique: false,
+      primaryKey: false,
+      keySchema: ""
+    }
+  }
+  
+  primaryKey() {
+    this.config.primaryKey = true
+    return this
+  }
+
+  notNull() {
+    this.config.notNull = true
+    return this
+  }
+
+  unique() {
+    this.config.unique = true
+    return this
+  }
+}
+
+export const integer = (name: string) => new Column(name, "integer")
+export const text = (name: string) => new Column(name, "text")
