206ff2ff869a8c9b45336460f9ea2436338d67b5
Miguel <adamastorpequenortx@gmail.com>
2026-05-28 05:05:20 -0300

    refactor: mover a execucao e mapeamento do select para o metodo execute

--- src/core/query.ts
+++ src/core/query.ts
@@ -34,6 +34,8 @@ export class Query {
   from(schemaTable: SchemaTable): Query {
     if (!schemaTable) throw new Error("Erro: O schema da table não foi especificado no 'from'");
 
+    this.tempSchemaTable = schemaTable
+    /*
     const tableName = schemaTable.__nameTable
     const table = this.database.tables[tableName]
 
@@ -68,7 +70,7 @@ export class Query {
     // reseta os valores
     this.tempKeys = []
     this.tempColumns = []
-
+    */
     return this
   }
 
@@ -213,17 +215,62 @@ export class Query {
     let result: unknown; 
 
     if (this.queryType === "SELECT") {
+      if (!this.tempSchemaTable) throw new Error("[Mist] Erro interno: O tempSchemaTable não existe ou é null");
+
+      const tableName = this.tempSchemaTable.__nameTable
+      const table = this.database.tables[tableName]
+
+      // Verifica se a tabela existe no banco
+      if (!table) throw new Error(`Erro: Tabela ${tableName} não existe`);
+
+      // Envia uma copia de todas as linhas da tabela se nenhuma coluna tiver sido especificada
+      if (this.tempColumns.length === 0) {
+        table.data.forEach(line => {
+          this.tempData.push({ ...line })
+        })
+
+        result = this.tempData
+
+        this.tempData = []
+        this.queryType = ""
+        this.tempSchemaTable = null
+
+        return result
+      }
+
+      // Monta as novas linhas
+      for (const line of table.data) {
+        const newLine: Record<string, any> = {}
+
+        for (let i = 0; i < this.tempColumns.length; i++) {
+          const key = this.tempKeys[i]
+          const tempColumn = this.tempColumns[i]
+          
+          if (key !== undefined && tempColumn !== undefined) {
+            newLine[key] = line[tempColumn]
+          }
+        }
+
+        this.tempData.push({ ...newLine }) // envia uma copia da linha
+      }
+
       result = this.tempData
 
-      this.tempData = []
+      // reseta os valores
+      this.tempKeys = []
+      this.tempColumns = []
+      this.tempSchemaTable = null
       this.queryType = ""
+      this.tempData = []
     }
+
 
     if (this.queryType === "INSERT") {
       result = "Dados inseridos com sucesso!"
 
       this.queryType = ""
     }
+
     return result
   }
 }
