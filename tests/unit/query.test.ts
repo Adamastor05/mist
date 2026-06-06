cdbfdae586075f0a00ac35c39e7ea15145758367
Miguel <adamastorpequenortx@gmail.com>
2026-06-06 15:18:53 -0300

    test: adicionar testes para update() e set()

--- tests/unit/query.test.ts
+++ tests/unit/query.test.ts
@@ -401,4 +401,67 @@ describe("Query", () => {
       })
     })
   })
+
+  describe("update e set", () => {
+
+    beforeEach(() => {
+      db.insert(users).values({ id: 1, name: "Miguel", age: 18, email: "miguel@gmail.com" }).execute()
+      db.insert(users).values({ id: 2, name: "Paulo", age: 25, email: "Paulo@gmail.com" }).execute()
+    })
+
+    describe("Casos de sucesso", () => {
+      it("deve atualizar o nome do usuario que tiver id igual a 1", () => {
+        const res = db
+        .update(users)
+        .set({ name: "Pedro" })
+        .where(eq(users.id, 1))
+        .execute()
+
+        expect(res).toBe("Dados atualizados com sucesso!")
+        expect(database.tables.users.data[0]).toEqual({ id: 1, name: "Pedro", age: 18, email: "miguel@gmail.com" })
+      })
+
+      it("deve atualizar o nome e o email do usuario que tiver id igual a 1", () => {
+        const res = db
+        .update(users)
+        .set({ name: "Pedro", email: "pedro@gmail.com" })
+        .where(eq(users.id, 1))
+        .execute()
+
+        expect(res).toBe("Dados atualizados com sucesso!")
+        expect(database.tables.users.data[0]).toEqual({ id: 1, name: "Pedro", age: 18, email: "pedro@gmail.com" })
+      })
+
+      it("deve atualizar o nome de todos os usuarios", () => {
+       const res = db
+        .update(users)
+        .set({ name: "Pedro" })
+        .execute()
+
+        expect(res).toBe("Dados atualizados com sucesso!")
+        expect(database.tables.users.data).toEqual([
+          { id: 1, name: "Pedro", age: 18, email: "miguel@gmail.com" },
+          { id: 2, name: "Pedro", age: 25, email: "Paulo@gmail.com" }
+        ])
+      })
+    })
+
+    describe("Cenarios de Erro", () => {
+
+      it("deve lançar erro se updade() for chamado sem schemaTable", () => {
+        expect(() => {
+          // @ts-ignore: Ignora o TypeScript para forçar o erro em tempo de execução
+          db.update().set({ name: "Pedro" }).execute()
+        }).toThrow("Erro: O schema da table não foi especificado no 'update'");
+        
+      })
+
+      it("deve lançar erro se set() for chamado sem values", () => {
+        expect(() => {
+          // @ts-ignore: Ignora o TypeScript para forçar o erro em tempo de execução
+          db.update(users).set().execute()
+        }).toThrow("Erro: Nenhum valor foi passado no 'set'")
+      })
+    })
+  })
 });
